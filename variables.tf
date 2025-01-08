646553a522772983f909829788d49048e96fd6ac
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-08 15:02:25 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "keyvault" {
+  description = "Name of keyvault"
+  type        = string
+}
+
+variable "resourcegroup" {
+  description = "The resource group of the Key Vault"
+  type        = string
+}
+
+variable "secretname" {
+  description = "name of secret"
+  type        = string
+}
+
+variable "secretvalue" {
+  description = "value of secret"
+  type        = string
+}
+
+variable "role_definition_name" {
+  description = "Name of role definition for secret"
+  type        = string
+  default    = NULL
+}
+
+variable "principal_id" {
+  description = "objectid of AAD group"
+  type        = string
+}
+
