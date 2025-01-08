2dc039d6785dfee94acb07d3952c7f38450ae018
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-08 19:10:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "azurerm_key_vault_secret" "secret" {
 
 resource "azurerm_role_assignment" "secretrole" {
   # Assign the Secret Officer role to the Azure AD group for each secret
-  principal_id       = var.role_definition_name 
+  principal_id       = var.principal_id
   role_definition_name = var.role_definition_name
   scope              = "${data.azurerm_key_vault.keyvault.id}/secrets/${azurerm_key_vault_secret.secret.name}" #azurerm_key_vault_secret.secret.id
 }
