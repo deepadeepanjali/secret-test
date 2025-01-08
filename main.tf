c00a80318c2176257c9a30d1080b7a73dbb2a3d3
deepadeepanjali <87965644+deepadeepanjali@users.noreply.github.com>
2025-01-08 14:58:01 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+}
+
+# Fetch keyvault details by keyvaut names
+data "azurerm_key_vault" "keyvault" {
+  name                = var.keyvault                 # Use each Key Vault name
+  resource_group_name = var.resourcegroup   # Provide the resource group name where the Key Vaults exist
+}
+
+resource "azurerm_key_vault_secret" "secret" {
+  name         = var.secretname 
+  value        =  var.secretvalue
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
+
+
+resource "azurerm_role_assignment" "secret_role" {
+  # Assign the Secret Officer role to the Azure AD group for each secret
+  principal_id       = var.role_definition_name 
+  role_definition_name = var.role_definition_name
+  scope              = azurerm_key_vault_secret.secret.id
+}
